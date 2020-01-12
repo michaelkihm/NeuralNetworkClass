56ec34e70e08e10a92135afff14f960818da3fcd
Michael Kihm <michael.kihm@web.de>
2020-01-12 09:27:07 +0100

    Added to_2d_np

--- helperfunctions.py
+++ helperfunctions.py
@@ -6,3 +6,21 @@ def permute_data(X, y):
     """
     perm = np.random.permutation(X.shape[0])
     return X[perm], y[perm]
+
+
+
+
+def to_2d_np(a: np.ndarray, 
+          type: str="col") -> np.ndarray:
+    '''
+    Turns a 1D Tensor into 2D
+    '''
+
+    assert a.ndim == 1, \
+    "Input tensors must be 1 dimensional"
+    
+    if type == "col":        
+        return a.reshape(-1, 1)
+    elif type == "row":
+        return a.reshape(1, -1)
+
